701ece5547325614d7ba0029d6098a703be29896
lzp <lizhaopeng.cn@gmail.com>
2017-10-24 23:31:50 +0800

    2

--- app/src/main/java/com/lzp/gitdemo/MainActivity.java
+++ app/src/main/java/com/lzp/gitdemo/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("log","1");
+        Log.i("log","2");
     }
 }
