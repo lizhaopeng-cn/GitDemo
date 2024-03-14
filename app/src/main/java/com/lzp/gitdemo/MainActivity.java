2ca57d854de97991eb41033a00884a2e5a9c253b
lizhaopeng <lizhaopeng.cn@gmail.com>
2024-03-14 14:29:52 +0800

    revert selected from other branch

--- app/src/main/java/com/lzp/gitdemo/MainActivity.java
+++ app/src/main/java/com/lzp/gitdemo/MainActivity.java
@@ -31,7 +31,6 @@ public class MainActivity extends Activity {
         Log.i("log","reset3");
         Log.i("log","reset4");
         Log.i("log","reset5");
-        Log.i("log","test");
         Log.i("log","cherry-pick-selected");
         Log.i("log","update");
     }
--- app/src/main/java/com/lzp/gitdemo/Test1.java
+++ app/src/main/java/com/lzp/gitdemo/Test1.java
@@ -1,7 +1,9 @@
 package com.lzp.gitdemo;
 
+import android.util.Log;
+
 public class Test1 {
     public static void main(String[] args){
-
+        Log.i("log","revert-selected");
     }
 }
